2f428973a781a92e05330ca91246bd7a428164d9
irwinet <irwinestradatorres@gmail.com>
2022-01-28 21:04:44 -0500

    Paquetes y Distribucion basica

---
+++ Fase 4 - Temas avanzados/Tema 11 - Modulos/Leccion 02 - Paquetes/paquete/adios/despedidas.py
@@ -0,0 +1,8 @@
+# Este es un modulo con funciones que saludan
+
+def despedirse():
+    print("Adios, me estoy despidiendo desde la funcion despedirse del modulo despedidas")
+
+class Despedida():
+    def __init__(self):
+        print("Adios, me estoy despidiendo desde el init de la clase Despedida")
---
+++ Fase 4 - Temas avanzados/Tema 11 - Modulos/Leccion 02 - Paquetes/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup 
+
+setup(
+    name="paquete",
+    version="0.1",
+    description="Este es un paquete de ejemplo",
+    author="Irwinet",
+    author_email="irwin@gmail.com",
+    url="http://irwin.com",
+    scripts=[],
+    packages=["paquete","paquete.adios","paquete.hola"]
+)
+
